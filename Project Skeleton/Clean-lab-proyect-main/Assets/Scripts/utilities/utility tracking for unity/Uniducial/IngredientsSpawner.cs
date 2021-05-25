4e55dcd459a8bd11528b78c7ae7b1646b03b1ebd
c2207 <82998174+c2207@users.noreply.github.com>
2021-05-25 11:32:57 +0200

    Arreglado el ingredients spawner

--- Project Skeleton/Clean-lab-proyect-main/Assets/Scripts/utilities/utility tracking for unity/Uniducial/IngredientsSpawner.cs
+++ Project Skeleton/Clean-lab-proyect-main/Assets/Scripts/utilities/utility tracking for unity/Uniducial/IngredientsSpawner.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class IngredientsSpawner : MonoBehaviour
 {
-    public List<GameObject> tomatoesList = new List<GameObject>();
-    public List<GameObject> meatList = new List<GameObject>();
+    public List<GameObject> ingredientsList = new List<GameObject>();
+    public float scaleFactor;
+    public Vector3 positionAdd;
 
-    public GameObject tomatoePrefab;
-    public GameObject meatPrefab;
+    public GameObject ingredientPrefab;
 
-    public void SpawnTomato()
+    private void OnCollisionEnter(Collision other)
     {
-        // La he puesto ahora a 0, pero tendría que aparecer la posición del personaje un poco movida para que parezca que lo está cogiendo
-        Vector3 position = new Vector3(0,0,0);
-        // Instanciamos un nuevo tomate
-        GameObject tomato = Instantiate(tomatoePrefab, position, tomatoePrefab.transform.rotation);
-        tomatoesList.Add(tomato);
-        Debug.Log("Tomato spawned");
-        //tomato.GetComponent<Tomato>().SetSpawner(this);
-    }
-
-    public void SpawnMeat()
-    {
-        // La he puesto ahora a 0, pero tendría que aparecer la posición del personaje un poco movida para que parezca que lo está cogiendo
-        Vector3 position = new Vector3(0, 0, 0);
-        GameObject meat = Instantiate(meatPrefab, position, meatPrefab.transform.rotation);
-        meatList.Add(meat);
-        //meat.GetComponent<Meat>().SetSpawner(this);
+        Vector3 position = new Vector3(75, 7, 85);
+        GameObject ingredient = Instantiate(ingredientPrefab, position, ingredientPrefab.transform.rotation);
+        // Instanciamos el ingrediente como un hijo del player para que asi lo siga
+        ingredient.transform.parent = other.transform;
+        ingredient.transform.position = other.transform.position + positionAdd;
+        ingredient.transform.localScale = new Vector3(1, 1, 1)*scaleFactor;
+        ingredientsList.Add(ingredient);
     }
 
     // Start is called before the first frame update
