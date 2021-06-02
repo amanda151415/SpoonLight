e5af611886596789560ea65c044bd35856fc7479
sardinaa <83430936+sardinaa@users.noreply.github.com>
2021-06-02 17:31:17 +0200

    Cortar ingredientes

---
+++ Spoon/Assets/Scripts/utilities/utility tracking for unity/Uniducial/CutIngredient.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutIngredient : MonoBehaviour
+{
+
+    public GameObject tomato_cutted;
+    public float scaleFactor;
+    public Vector3 positionAdd;
+
+    public float cuttime = 5.0f;
+
+
+
+
+    private void OnCollisionStay(Collision other)
+    {
+
+        GameObject child = null;
+
+
+        GameObject collider = other.gameObject;
+        Transform collider_t = other.transform;
+        // Buscamos si hay algun child en el player con el que hemos colisionado que corresponda a un ingrediente
+        foreach (Transform child_t in collider_t)
+        {
+            if (child_t.tag == "Ingredient")
+            {
+                child = child_t.gameObject;
+            }
+        }
+
+        // Si existe, lo eliminamos
+        if (child != null)
+        {
+            if (cuttime > 0.0f)
+            {
+                cuttime -= Time.deltaTime;
+            }
+            
+            if (cuttime < 0.0f)
+            {
+
+                Vector3 position = new Vector3(75, 7, 85);
+
+                GameObject ingredient = Instantiate(tomato_cutted, child.transform.position, Quaternion.identity);
+                // Instanciamos el ingrediente como un hijo del player para que así lo siga
+                ingredient.transform.parent = other.transform;
+                ingredient.transform.position = other.transform.position + positionAdd;
+                ingredient.transform.localScale = new Vector3(1, 1, 1) * scaleFactor;
+                Destroy(child);
+
+                
+                Debug.Log("Entro");
+                
+
+                cuttime = 0.0f;
+          
+            }
+            
+
+
+
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        cuttime = 5.0f;
+        Debug.Log("Surto");
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
