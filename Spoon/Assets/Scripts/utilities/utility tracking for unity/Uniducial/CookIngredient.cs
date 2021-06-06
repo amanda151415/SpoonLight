c848b8d3c7845b13b796d85990f2babb4808143c
c2207 <82998174+c2207@users.noreply.github.com>
2021-06-06 08:56:32 +0200

    Acciones cooking + cambiar color ingredientes

--- Spoon/Assets/Scripts/utilities/utility tracking for unity/Uniducial/CookIngredient.cs
+++ Spoon/Assets/Scripts/utilities/utility tracking for unity/Uniducial/CookIngredient.cs
@@ -11,6 +11,7 @@ public class CookIngredient : MonoBehaviour
 
     private float cookTime = 5.0f;
     private bool isCooking = false;
+    private GameObject cooking_ingredient;
 
     private void OnCollisionEnter(Collision other)
     {
@@ -24,15 +25,52 @@ public class CookIngredient : MonoBehaviour
             // Cocinamos solo si es un ingrediente. No nos interesa cocinar elementos cortados, cocinados o quemados 
             if (child_t.tag == "Ingredient" && !isCooking)
             {
+                // Instanciamos el child en la variable cooking_ingredient, para así poder recoger el ingrediente al volver
+                cooking_ingredient = GameObject.Instantiate(child_t.gameObject);
                 isCooking = true;
                 Debug.Log("Cooking");
+                child = child_t.gameObject;
+                Destroy(child);
                 //cookTime -= Time.deltaTime;
                 //// Convertimos a segundos
                 //float sec = cookTime % 60;
             }
         }
         if (isCooking && child == null) {
-            // Si ya estamos cocinando y el player no lleva ningun ingrediente la idea sería recogerlo con la tag ya cambiada.
+            // Si ya estamos cocinando y el player no lleva ningun ingrediente la idea es recogerlo con la tag ya cambiada.
+
+            if (cookTime < -5.0f)
+            {
+                // Cambiamos el color para que parezca quemado
+                // El material en el que tenemos que cambiar el color es un child del ingrediente, por eso tenemos que hacer este GetChild(0)
+                var ingredient_renderer = cooking_ingredient.transform.GetChild(0).GetComponent<Renderer>();
+                ingredient_renderer.material.color = Color.black;
+                // Cambiamos el tag
+                cooking_ingredient.tag = "OvercookedIngredient";
+                Debug.Log(cooking_ingredient.tag);
+                cooking_ingredient.transform.parent = other.transform;
+                cooking_ingredient.transform.position = other.transform.position + positionAdd;
+                cooking_ingredient.transform.localScale = new Vector3(1, 1, 1) * scaleFactor;
+
+                // Reiniciamos contador
+                cookTime = 5.0f;
+                isCooking = false;
+            }
+            else if (cookTime < 0.0f)
+            {
+                //cooking_ingredient.renderer.material.color = new Color(1, 1, 1);
+                var ingredient_renderer = cooking_ingredient.transform.GetChild(0).GetComponent<Renderer>();
+                ingredient_renderer.material.color = Color.blue;
+                cooking_ingredient.tag = "CookedIngredient";
+                Debug.Log(cooking_ingredient.tag);
+                cooking_ingredient.transform.parent = other.transform;
+                cooking_ingredient.transform.position = other.transform.position + positionAdd;
+                cooking_ingredient.transform.localScale = new Vector3(1, 1, 1) * scaleFactor;
+
+                // Reiniciamos contador
+                cookTime = 5.0f;
+                isCooking = false;
+            }
         }
         else {
             Debug.Log("sonidito");
@@ -118,5 +156,10 @@ public class CookIngredient : MonoBehaviour
     void Update()
     {
         // Si isCooking esta activado, restamos tiempo
+        if (isCooking)
+        {
+            cookTime -= Time.deltaTime;
+            Debug.Log(cookTime);
+        }
     }
 }
